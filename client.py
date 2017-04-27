0789d020810aa71778acccb315dd2bd9c3a1eace
Brent Frederisy <brentfred@Brents-MacBook-Pro.local>
2017-04-27 09:57:39 -0700

    Add client and receiver module

---
+++ client.py
@@ -0,0 +1,62 @@
+"""
+	Murmur
+	~~~~~~
+
+	Client
+"""
+import socket
+
+from threading import Thread
+
+from message_receiver import MessageReceiver, Message
+
+class Client:
+	"""
+	Processes received messages from a server and sends messages to the server.
+	"""
+	def __init__(self, server_ip, process=print, port=None):
+		"""
+		Initializes the client.
+
+		server_ip - the server's ip address.
+		port - the reception port of the server and the clients on the channel.
+		process - properly received messages are passed to this function.
+		"""
+		self.process = process
+		self.receiver = MessageReceiver(port)
+		self.server_ip = server_ip
+		self.channel_port = port
+
+		Thread(name="Client Listening Thread", 
+			target=self._process_messages, daemon=True).start()
+
+	def _process_messages(self):
+		"""
+		Retrieves messages from a MessageReceiver object and passes it to the
+		process function. Should be used in a daemon thread as it loops 
+		forever.
+		"""
+		for received_message in self.receiver:
+			if received_message.sender == self.server_ip or received_message.sender == 'local':
+				self.process(received_message.body)
+
+	def connect(self, username):
+		"""
+		Connects the client to a server.
+
+		username - this client's username
+		"""
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+			sock.connect((self.server_ip, self.channel_port))
+			sock.send("/regi {}".format(username).encode())
+
+	def send(self, message):
+		"""
+		Sends a message to the server.
+
+		message - some string to send to the server.
+		"""
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+			sock.connect((self.server_ip, self.channel_port))
+
+			sock.send(message.encode())
---
+++ message_receiver.py
@@ -0,0 +1,74 @@
+"""
+	Murmur
+	~~~~~~
+
+	Message Receiver
+"""
+
+import socket
+
+from threading import Thread
+from queue import Queue
+from collections import namedtuple
+
+SHORT_TIME_OUT = 3.0
+MAX_BUFFER_SIZE = 4096
+
+Message = namedtuple("Message", "body sender")
+
+class MessageReceiver:
+	"""
+	A object to manage messages sent to a Murmer client or server. Messages
+	should be retrieved by using this object as a generator expression
+	"""
+	message_queue = Queue()
+
+	def __init__(self, port=None):
+		"""
+		Sets up and starts the receiver for listening. Takes the port as
+		a parameter. The port should be the same for all clients and servers.
+
+		port - the receiving port for the listening socket.
+		"""
+		if port is not None:
+			self.channel_port = port
+
+			Thread(name="Listening Thread", target=self._receive_messages, 
+				daemon=True).start()
+
+	def __iter__(self):
+		"""
+		Generator that produces messages from the message queue.
+
+		returns a Message tuple
+		"""
+		yield self.message_queue.get()
+
+	def receive(self, new_message):
+		"""
+		Puts a message on the message queue.
+
+		message - a Message tuple
+		"""
+		if not isinstance(new_message, Message):
+			raise ValueError
+		
+		self.message_queue.put(new_message)
+
+	def _receive_messages(self):
+		listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(SHORT_TIME_OUT)
+
+		listening_sock.bind(('0.0.0.0', self.channel_port)) #TODO FIX THIS
+		listening_sock.listen(5)
+
+		while True:
+			try:
+				client_sock, client_addr = listening_sock.accept()
+				body = client_sock.recv(MAX_BUFFER_SIZE).decode()
+				
+				if len(body) > 0:
+					self.receive(Message(body, client_addr[0]))
+
+			except socket.timeout:
+				pass
